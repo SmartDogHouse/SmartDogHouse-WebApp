07729e8d03a25aa33265716ffb08c8693890472a
camillo2008 <jordancaminati@libero.it>
2021-06-24 04:21:53 -0700

    adde main views

---
+++ src/views/Home.vue
@@ -0,0 +1,84 @@
+<template >
+  <div>
+    <v-app-bar app color="grey" flat>
+      <v-tabs centered class="ml-n9" color="orange darken-4">
+        <v-tab v-for="link in links" :key="link">
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+      <v-app-bar-nav-icon
+        color="orange darken-4"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="no-active">
+          <v-list-item class="noActive" @click.stop="logOut">
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="grey">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="5">
+            <v-sheet rounded="lg" min-height="268">
+              <dogScroller :names="names" :codes="codes"></dogScroller>
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="7">
+            <v-sheet rounded="lg" min-height="268">
+              <dogView :names="names" :codes="codes"></dogView>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    //  StatsChart: () => import("../components/StatsChart"),
+
+    dogScroller: () => import("../components/DogScroller"),
+    dogView: () => import("../components/DogView"),
+  },
+  data: () => ({
+    names: ["Lana", "Merinox", "Puppa"],
+    codes: ["11222", "33344", "67999"],
+    drawer: false,
+    group: null,
+    links: ["Gestore", "Addetto cibo", "Altro addetto", "Addetto alla salute"],
+  }),
+  props: {},
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+  methods: {
+    logOut() {
+      this.$router.push("/Home").catch(() => {});
+
+      this.axios.get("https://httpbin.org/get").then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.noActive::before {
+  opacity: 0;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="mx-auto">
+          <v-img :aspect-ratio="16 / 9" src="/assets/Smh0.png"></v-img>
+          <v-spacer> </v-spacer>
+          <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <LoginForm></LoginForm>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    LoginForm: () => import("../components/LoginForm.vue"),
+  },
+};
+</script>
